c66a107db5229afc88eeef6f22a78b91a0bef89f
Petra Lamborn <petra@petras.space>
2021-01-05 21:54:52 +1300

    Fix -1 bug; flake8

--- rti.py
+++ rti.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect
 from flask_table import Table, Col, LinkCol
-from datetime import datetime
 from dateutil.parser import isoparse
 import requests
 import re
@@ -9,7 +8,8 @@ from urllib.parse import quote
 depStatus = {
     "onTime": "On time",
     "early": "Early",
-    "delayed": "Late"
+    "delayed": "Late",
+    "cancelled": "Cancelled"
 }
 
 
@@ -23,8 +23,8 @@ class TimeTable(Table):
 
 
 class SearchTable(Table):
-    code = LinkCol("Code", "timetable", url_kwargs=dict(stop="sms"), attr =
-                   'code')
+    code = LinkCol("Code", "timetable", url_kwargs=dict(stop="sms"),
+                   attr='code')
     stop = Col("Stop")
     table_id = "searchtable"
 
@@ -38,6 +38,10 @@ searchurl = "https://www.metlink.org.nz/api/v1/StopSearch/"
 def stopExtract(code, name):
     z = re.match("{} - (.*)".format(code), name)
     return z.groups()[0]
+
+
+def minCompare(comp, origin):
+    return 0 if comp <= origin else (comp - origin).seconds // 60
 
 
 @app.route("/")
@@ -59,23 +63,24 @@ def timetable(stop):
     req = requests.get("{}{}".format(stopurl, stop))
     if req.status_code != 200:
         # return "Error {}".format(req.status_code)
-        return render_template("nostop.html", error = req.status_code)
+        return render_template("nostop.html", error=req.status_code)
     # return req.json()
     rv = req.json()
     lastup = isoparse(rv["LastModified"])
-    ttdat = [{"route" : s["ServiceID"], "dest" : s["DestinationStopName"],
-              "sched" :
+    ttdat = [{"route": s["ServiceID"], "dest": s["DestinationStopName"],
+              "sched":
               isoparse(s["AimedDeparture"]).strftime("%H:%M"),
-              "est" : "" if s["ExpectedDeparture"] is None else "{} mins".format((isoparse(s["ExpectedDeparture"]) -
-                       lastup).seconds // 60),
-              "status" : depStatus[s["DepartureStatus"]] if
+              "est": "" if s["ExpectedDeparture"] is None else
+                  "{} mins".format(minCompare(isoparse(s["ExpectedDeparture"]),
+                                              lastup)),
+              "status": depStatus[s["DepartureStatus"]] if
               s["DepartureStatus"] in depStatus else s["DepartureStatus"]}
              for s in rv["Services"]]
     tTable = TimeTable(ttdat)
-    return render_template("stop.html", stopnumber = stop,
-                           stopname = rv["Stop"]["Name"],
-                           lup = lastup.strftime("%H:%M:%S, %A %B %-d"),
-                          table = tTable if len(ttdat) > 0 else "")
+    return render_template("stop.html", stopnumber=stop,
+                           stopname=rv["Stop"]["Name"],
+                           lup=lastup.strftime("%H:%M:%S, %A %B %-d"),
+                           table=tTable if len(ttdat) > 0 else "")
 
 
 @app.route("/search/")
@@ -83,14 +88,15 @@ def stopsearch():
     query = request.args["q"] if "q" in request.args else ""
     req = requests.get("{}{}".format(searchurl, quote(query)))
     if req.status_code != 200:
-        return render_template("badsearch.html", error = req.status_code)
-    stdat = [{"code" : s["Sms"], "sms" : s["Sms"], "stop" :
+        return render_template("badsearch.html", error=req.status_code)
+    stdat = [{"code": s["Sms"], "sms": s["Sms"], "stop":
               stopExtract(s["Sms"], s["Name"])} for s
              in req.json()]
     sTable = SearchTable(stdat)
     # return sTable.__html__()
-    return render_template("search.html", searchstring = query, numres =
-                           len(stdat), table = sTable if len(stdat) > 0 else "")
+    return render_template("search.html", searchstring=query,
+                           numres=len(stdat),
+                           table=sTable if len(stdat) > 0 else "")
 
 
 if __name__ == "__main__":
